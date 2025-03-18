7441e9b8861fce86e998b05abbdc401f03b8b179
Maksim Chalyh <nazgulius@yandex.ru>
2025-03-18 18:12:40 +0300

    layout start

---
+++ src/components/Store.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import products from './products '
+
+export default function Store () {  
+  const [filterP, setFilter] = useState("All");
+
+  onSwitch() {} // обработчик события нажатия
+
+  IconSwitch(icon) {} // переключение иконок
+
+
+  return (
+    <>      
+      <div className='toolbar'>
+        
+      </div>
+      <div className='item-lists'>
+        
+      </div>
+    </>
+  );
+}
