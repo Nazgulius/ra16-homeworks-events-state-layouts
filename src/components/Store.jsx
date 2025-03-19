9735929f0d93908d9232f05f64eaf6928b10c791
Maksim Chalyh <nazgulius@yandex.ru>
2025-03-19 11:34:46 +0300

    view module OK, view list in progress

--- src/components/Store.jsx
+++ src/components/Store.jsx
@@ -1,22 +1,82 @@
 import { useState } from 'react';
-import products from './products '
+import products from './products'
+import viewList from './icons/view_list.png'
+import viewModule from './icons/view_module.png'
 
 export default function Store () {  
-  const [filterP, setFilter] = useState("All");
+  const iconsList = [
+    {
+      name: "view_list", 
+      img: viewList, 
+    },
+    {
+      name: "view_module", 
+      img: viewModule, 
+    },
+  ];
 
-  onSwitch() {} // обработчик события нажатия
+  const [menu, setItems] = useState("view_list");
 
-  IconSwitch(icon) {} // переключение иконок
+  // обработчик события нажатия
+  const onSwitch = (icon) => {
+    setItems(icon);
+  } 
 
+  // отображение списка товаров в зависимости от выбранного меню  
+  const renderItems = () => {  
+    if (menu === "view_module") {
+      return products.map((item) => (  
+        <div className={`item ${menu}`} key={item.img} style={{  
+          backgroundImage: `url(${item.img})`,  
+          backgroundSize: 'cover',   
+        }}>  
+          <h3 className='item-name'>{item.name}</h3>  
+          <span className='item-color'>{item.color}</span>            
+          <div className='item-price-btn'>  
+            <span className='item-price'>${item.price}</span>  
+            <button className='item-btn-atc' onClick={() => console.log(`ADD TO CART: ${item.name} - $${item.price}`)} >ADD TO CART</button>  
+          </div>  
+        </div>  
+      ));  
+    } else if (menu === "view_list") {
+      return products.map((item) => (  
+        <div className={`item ${menu}`} key={item.img} style={{ 
+        }}>  
+        <img src={item.img} alt={item.name} className='item-img'/>
+          <h3 className='item-name'>{item.name}</h3>  
+          <span className='item-color'>{item.color}</span>            
+          <span className='item-price'>${item.price}</span>  
+          <button className='item-btn-atc' onClick={() => console.log(`ADD TO CART: ${item.name} - $${item.price}`)} >ADD TO CART</button>  
+        </div>  
+      ));  
+    } else {
+      console.log("Ошибка! Иконка меню не выбрана!");
+    }
+    
+  }   
 
-  return (
+  return (  
     <>      
-      <div className='toolbar'>
-        
-      </div>
-      <div className='item-lists'>
-        
-      </div>
-    </>
-  );
+      <div className='toolbar'>  
+        {iconsList.map((icons) => (  
+          <button   
+            key={icons.name}   
+            onClick={() => onSwitch(icons.name)}   
+            className='btn'  
+            style={{  
+              backgroundImage: `url(${icons.img})`,   
+              backgroundSize: 'cover',   
+              width: '50px',  
+              height: '50px',  
+            }}  
+          >  
+          </button>  
+        ))}   
+      </div>  
+      <div className={`item-lists ${menu}`}>  
+        {renderItems()}   
+      </div>  
+    </>  
+  );  
+
 }
